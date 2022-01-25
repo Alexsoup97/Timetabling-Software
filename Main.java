b8c896a932bb634babea5a8ed9e3f4055e975578
Alex Souphanthong <alexsoup8@gmail.com>
2022-01-24 20:04:54 -0500

    updates

--- Main.java
+++ Main.java
@@ -9,14 +9,19 @@ public class Main {
 // System.out.println(Data.roomMap);
 // System.out.println(Data.roomTypeCourses);
 // System.out.println(Data.roomTypeMap)
+
 // System.out.println(Data.studentMap);
 
+        long time = System.nanoTime();
       
         SpecialCourseScheduler s = new SpecialCourseScheduler();
+        
         CourseScheduler courseScheduler = new CourseScheduler(s);
-        ArrayList<ClassInfo> timetable =  courseScheduler.getNewTimetable();
-        StudentAssignment studentAssignment =  new StudentAssignment(timetable);
-        UserInterface UI = new UserInterface(timetable);
+
+        System.out.println(System.nanoTime() - time);
+        // ArrayList<ClassInfo> timetable =  courseScheduler.getNewTimetable();
+        // StudentAssignment studentAssignment =  new StudentAssignment(timetable);
+        UserInterface UI = new UserInterface(null);
         
        //System.out.println(courseScheduler.getNewTimetable());
         
